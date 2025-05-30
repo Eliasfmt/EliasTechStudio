ff2c784cfc63584b5d78a5ce235b9a9d84a9b4e5
Elias Munoz <eliasfmt@gmail.com>
2025-05-29 21:34:30 -0500

    Add sitemap.xml and next-sitemap setup

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://em-tech.ai',
+  generateRobotsTxt: true,
+  outDir: './public', // 👈 where to save sitemap.xml and robots.txt
+}
